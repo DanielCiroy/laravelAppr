5fccdfe1dbfc580ee975200cbe785f0c391ac771
Daniel ciroy <142291477+DanielCiroy@users.noreply.github.com>
2024-04-05 13:56:00 +0200

    suite sur les route: recup avec get

---
+++ CourLaravel/resources/views/apropos.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="padding=100">
+    <h1>a propos de nous</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis molestias impedit facere exercitationem itaque obcaecati debitis quis unde quos nulla tenetur animi sequi natus rerum, eum dolore quae qui maiores quo ipsa sit libero maxime rem accusantium. Sint architecto cumque labore dignissimos perferendis adipisci ex natus cupiditate quis voluptatibus ducimus numquam quos quibusdam accusamus laudantium, maxime cum hic esse illum nesciunt asperiores expedita blanditiis voluptatum. Nemo voluptate quasi natus fugit fuga? Tenetur minima eveniet corporis doloremque corrupti labore nemo aperiam animi fugit, rerum aut dicta atque beatae, architecto ipsam consectetur enim eius. Ad ipsa officiis, non eius perferendis alias natus.</p>
+</body>
+</html>
--- CourLaravel/routes/web.php
+++ CourLaravel/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/',function(){
     return view('welcome');
+});
+
+Route::get('/salut/{name}/{age}',function($nom,$age){
+   echo 'salut tout le monde je suis '.$nom.'et j\'ai '.$age.'ans';
+});
+
+Route::get('/about',function(){
+    return view('apropos');
 });
